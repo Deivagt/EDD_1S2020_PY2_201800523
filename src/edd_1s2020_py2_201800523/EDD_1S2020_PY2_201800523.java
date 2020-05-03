8127d13fe4c0348bbe20cc2c481ac025dac723bf
Deivagt <58482545+Deivagt@users.noreply.github.com>
2020-05-02 20:45:19 -0600

    Hello

---
+++ src/edd_1s2020_py2_201800523/EDD_1S2020_PY2_201800523.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edd_1s2020_py2_201800523;
+
+/**
+ *
+ * @author David
+ */
+public class EDD_1S2020_PY2_201800523 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+	// TODO code application logic here
+    }
+    
+}
